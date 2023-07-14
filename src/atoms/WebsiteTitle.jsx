63e3ca2a1f68a7bcca90393563d87d9a4a97f168
Troy Tomasch <troyrh6@gmail.com>
2023-07-14 17:26:12 -0400

    Deploying

--- src/atoms/WebsiteTitle.jsx
+++ src/atoms/WebsiteTitle.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import longlogo from "../images/longlogo.png";
 
 // Component for the main title of the website
 const WebsiteTitle = () => {
+  // Will make this a logo at some point
+
   return (
-    <div className="m-2">
-      <img src={longlogo} alt="Troy Tomasch Logo" className="w-96 h-36"></img>
+    <div className="m-4">
+      <h1 className="text-6xl font-raleway text-slate-600 drop-shadow-md">
+        Troy Tomasch
+      </h1>
+      {/* <img src={longlogo} alt="Troy Tomasch Logo" className="w-96 h-36"></img> */}
     </div>
   );
 };
