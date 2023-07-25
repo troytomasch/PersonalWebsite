82dfb059f31cbdc0c29156ac7eec375bcf1f5a4d
Troy Tomasch <troyrh6@gmail.com>
2023-07-25 17:03:08 -0400

    Adding frontend data verification
    - Verified experiences data
    - Verified projects data
    - Also updated some minor homepage data and added some comments

--- src/organisms/Experiences.jsx
+++ src/organisms/Experiences.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import ExperienceCard from "../molecules/ExperienceCard";
 import { experiences } from "../data";
+import { verifyExperiences } from "../utilities/verifyData";
+import SubheaderText from "../atoms/SubheaderText";
 
 // Component for the experiences list
 const Experiences = () => {
+  // Verifying data is as expected
+  try {
+    verifyExperiences(experiences);
+  } catch (e) {
+    // Would want to add some sort of error logging here to keep track of this
+    console.log(e);
+
+    return (
+      <div className="m-5 flex gap-7 flex-col">
+        <SubheaderText text={"Could not display experiences"} />
+      </div>
+    );
+  }
+
   return (
     <div className="m-5 flex gap-7 flex-col">
       {experiences.map((experience) => {
--- src/organisms/ProjectsLayout.jsx
+++ src/organisms/ProjectsLayout.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import ProjectCard from "../molecules/ProjectCard";
+import SubheaderText from "../atoms/SubheaderText";
 import { projects } from "../data";
+import { verifyProjects } from "../utilities/verifyData";
 
+// Component for the layout of the projects tab
 const Projects = () => {
+  // Verifying data is as expected
+  try {
+    verifyProjects(projects);
+  } catch (e) {
+    // Would want to add some sort of error logging here to keep track of this
+    console.log(e);
+
+    return (
+      <div className="m-5 flex gap-7 flex-col">
+        <SubheaderText text={"Could not display projects"} />
+      </div>
+    );
+  }
+
   return (
     <div className="m-5 flex gap-7 flex-col">
       {projects.map((project) => {
